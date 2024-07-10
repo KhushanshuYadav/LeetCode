fbd1e1f7d4332a4e79b7a3e71ca273adf445a9ef
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-07-10 22:29:48 +0530

    Time: 0 ms (100%), Space: 11.2 MB (80.59%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -20,40 +20,63 @@ public:
 */
 
 class Solution {
-    private:
-    void dfs(Node*oNode,Node*cNode,unordered_map<Node*,Node*>& m){
+private:
+    void dfs(Node* oNode, Node* cNode, unordered_map<Node*, Node*>& m) {
 
-        for(Node*oAdjNode:oNode->neighbors){
+        for (Node* oAdjNode : oNode->neighbors) {
 
-            if(m.count(oAdjNode)==0){
-                //as adj is not in map so create deep copy of adj
-                Node*cAdjNode=new Node(oAdjNode->val);
-                m[oAdjNode]=cAdjNode;
+            if (m.count(oAdjNode) == 0) {
+                // as adj is not in map so create deep copy of adj
+                Node* cAdjNode = new Node(oAdjNode->val);
+                m[oAdjNode] = cAdjNode;
                 cNode->neighbors.push_back(cAdjNode);
-                dfs(oAdjNode,cAdjNode,m);
-            }
-            else{
+                dfs(oAdjNode, cAdjNode, m);
+            } else {
 
                 cNode->neighbors.push_back(m[oAdjNode]);
-
             }
-
         }
-
     }
+
 public:
     Node* cloneGraph(Node* node) {
 
-        if(!node) return nullptr;
+        if (!node)
+            return nullptr;
 
-        if(node->neighbors.empty()) return new Node(node->val);
+        if (node->neighbors.empty())
+            return new Node(node->val);
 
-        unordered_map<Node*,Node*>m; //old to new
-        //since we need to return deep copy so we need to clone all adj nodes and push them into correct node neighbors vector 
-        Node*clonedNode=new Node(node->val);
-        m[node]=clonedNode;
-        dfs(node,clonedNode,m);
+        unordered_map<Node*, Node*> m; // old to new
+        // since we need to return deep copy so we need to clone all adj nodes
+        // and push them into correct node neighbors vector
+        Node* clonedNode = new Node(node->val);
+        m[node] = clonedNode;
+
+        // dfs(node,clonedNode,m);
+
+        // Below BFS;
+        queue<Node*> q; // pushing oNode as we need to fill cloned neighbors so
+                        // crate copy of oNodes is bfs manner
+        q.push(node);
+        while (!q.empty()) {
+            Node* oNode = q.front();
+            q.pop();
+
+            for (Node* oAdjNode : oNode->neighbors) {
+                if (m.count(oAdjNode) == 0) {
+                    // as adj is not in map so create deep copy of adj
+                    Node* cAdjNode = new Node(oAdjNode->val);
+                    m[oAdjNode] = cAdjNode;
+                    m[oNode]->neighbors.push_back(cAdjNode);
+                    q.push(oAdjNode);
+                } else {
+
+                    m[oNode]->neighbors.push_back(m[oAdjNode]);
+                }
+            }
+        }
+
         return clonedNode;
-        
     }
 };
