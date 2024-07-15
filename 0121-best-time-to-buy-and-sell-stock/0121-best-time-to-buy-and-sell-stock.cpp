3f56740c0c53d66bea48c7dfc96bf278f5544de3
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-07-15 21:16:46 +0530

    Time: 92 ms (31.66%), Space: 95.8 MB (35.61%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+
+    //code says buy at minimum price so if currr< min update min
+    //profit will be max of max profit annd curr profit
+    int maxProfit=0;
+    int minPrice=INT_MAX;
+     
+    for(int i=0;i<n;i++){
+        minPrice=min(minPrice,arr[i]);
+        int profit=arr[i]-minPrice;
+        maxProfit=max(maxProfit,profit);
+    }
+     return maxProfit;
+    }
+};
