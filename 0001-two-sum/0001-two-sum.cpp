bbbd5d14604112be4084aaac41bd489e2d895b85
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-07-15 23:16:45 +0530

    Time: 9 ms (68.46%), Space: 14.4 MB (12.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& book, int k) {
-        unordered_map<int,int>s;
+        unordered_map<int,int>m;
         int n=book.size();
         vector<int>v;
     
 
     for(int i=0;i<n;i++){
         
-        if(s.find(k-book[i])!=s.end()){
+        if(m.find(k-book[i])!=m.end()){
             v.push_back(i);
-            v.push_back(s[k-book[i]]);
+            v.push_back(m[k-book[i]]);
             break;
         }
-        s[book[i]]=i;
+        m[book[i]]=i;
 
     }
 
