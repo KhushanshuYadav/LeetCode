d630b79415ebf0d95fa91d110fd0131660d63d0f
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-07-16 16:24:26 +0530

    Time: 4 ms (42.68%), Space: 8.8 MB (88.71%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void fun(int begin,vector<int>& nums,vector<vector<int>>&ans){
+        if(begin==nums.size()-1){
+            ans.push_back(nums);
+            return;
+        }
+
+        for(int i=begin;i<nums.size();i++){  //we fix each i and begin position
+            swap(nums[begin],nums[i]); //swap i.e fix ith and begin
+            fun(begin+1,nums,ans);     //call for next not fixed half
+            swap(nums[begin],nums[i]); //backtrack
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+
+        fun(0,nums,ans);
+        //begin == fixing position
+
+        return ans;
+    }
+};
