20b659d808d82e789138940dd985e50cddcfd31a
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-08-11 00:18:42 +0530

    Time: 8 ms (46.82%), Space: 30.8 MB (16.11%) - LeetHub

---
+++ 3494-minimum-cost-for-cutting-cake-i/3494-minimum-cost-for-cutting-cake-i.cpp
@@ -0,0 +1,72 @@
+class Solution {
+public:
+    int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+
+        sort(horizontalCut.rbegin(),horizontalCut.rend());
+        sort(verticalCut.rbegin(),verticalCut.rend());
+
+        int horizontalBCount=1;
+        int verticalBcount=1;
+
+        int i=0;
+        int j=0;
+        int ans=0;
+
+        while(i<m-1 && j<n-1){
+
+            if(horizontalCut[i]>verticalCut[j]){
+                //we made horizontal cuts on all vertcal blocks
+
+                ans+=(horizontalCut[i]*verticalBcount);
+
+                //number of horizontal blocks will inc
+                horizontalBCount++;
+
+
+
+                i++;
+            }
+            else{
+                //we made vertical cuts on all horizontal blocks
+
+                ans+=(verticalCut[j]*horizontalBCount);
+
+                //number of vertical blocks will inc
+                verticalBcount++;
+
+
+
+                j++;
+            }
+        }
+
+        while(i<m-1){
+
+            ans+=(horizontalCut[i]*verticalBcount);
+            horizontalBCount++;
+
+
+
+            i++;
+
+        }
+
+        while(j<n-1){
+
+            ans+=(verticalCut[j]*horizontalBCount);
+            verticalBcount++;
+
+
+
+            j++;
+
+        }
+
+
+
+
+
+        return ans;
+        
+    }
+};
