a33a9cc3c99183f79d7d4869ef7b3f9b5d6b6350
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-08-10 13:39:52 +0530

    Time: 0 ms (100%), Space: 8.1 MB (11.46%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
     //returns total path from cell i,j to cell m-1,n-1 by following constraints of question
 
-    //BELOW CODE is from m-1,n-1 to 0,0 i.e reverse
+    //BELOW CODE is from m-1,n-1 to 0,0 i.e reverse i.e path to m,n from 0,0
     int solve(int m,int n,vector<vector<int>>& dp){
 
         if(m==0 && n==0){
@@ -25,8 +25,36 @@ public:
         if(m==1 && n==1) return 1;
 
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+        //return solve(m-1,n-1,dp);
         //tabulation
+
+        //filling base case
+        dp[0][0]=1;
+
+        for(int col=0;col<n;col++){
+            dp[0][col]=1;
+
+        }
+
+        for(int row=0;row<m;row++){
+            dp[row][0]=1;
+            
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                int ans=0;
+                if(j-1>=0) ans+=dp[i][j-1];
+                if(i-1>=0) ans+=dp[i-1][j];
+
+                dp[i][j]=ans;
+            }
+        }
+
+        return dp[m-1][n-1];
+        
+
+        
 
         
     }
