716236b56418ca765f468aafa6ed2eaa5d41a6a8
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-08-10 21:09:05 +0530

    Time: 7 ms (61.1%), Space: 12.5 MB (75.23%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -26,9 +26,58 @@ public:
         int rows=grid.size();
         int cols=grid[0].size();
 
-        vector<vector<int>>dp(rows,vector<int>(cols,-1));
+       
 
-        return solve(rows-1,cols-1,grid,dp);
+        //return solve(rows-1,cols-1,grid,dp);
+
+        //Tabulation;
+
+        vector<int>prevRow(cols,-1);
+        //each cell i,j stores min path sum from 0,0 to this cell reached by problem constraints
+
+        //from 0,0 to 0,0 sum will be itself only
+        prevRow[0]=grid[0][0]; 
+
+        
+        //on each cell in below loop we can reach from left cell only so cumalative sum of it 
+        for(int col=1;col<cols;col++){
+            prevRow[col]=grid[0][col]+prevRow[col-1];
+        }
+
+        //on each cell in below loop we can reach from top cell only so cumalative sum of it 
+        /*for(int row=1;row<rows;row++){
+            dp[row][0]=grid[row][0]+dp[row-1][0];
+        }*/
+
+        for(int i=1;i<rows;i++){
+
+            prevRow[0]=grid[i][0]+prevRow[0];
+
+            
+
+            for(int j=1;j<cols;j++){
+
+                //we require the min of sum of current state and left or above state to store in current cell
+
+                int downSum=grid[i][j]+prevRow[j-1];
+                int rightSum=grid[i][j]+prevRow[j];
+
+                int ans=min(downSum,rightSum);
+
+                prevRow[j]=ans;
+                
+
+
+
+                
+            }
+
+            
+        }
+
+        return prevRow[cols-1];
+
+        
         
     }
 };
