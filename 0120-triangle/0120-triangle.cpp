4e9bf64fda2fa6366c9ccc85e08164da06f240bf
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-08-11 14:58:17 +0530

    Time: 0 ms (100%), Space: 11.8 MB (20.66%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -20,8 +20,55 @@ public:
 
         int n = triangle.size();
 
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        
 
-        return solve(0, 0, n, triangle,dp);
+        //return solve(0, 0, n, triangle,dp);
+
+        //SPACE  OPTIMIZATION
+
+        //in table
+        //each cell i,j stores min path sum to reach cell i,j from cell 0,0
+        vector<int>prevRow;
+
+        //filling base cases below
+
+        //to each cell (i,j) we can arrive from UP (i-1,j) or UP-LEFT (i-1,j) cell  
+        
+
+       prevRow.push_back(triangle[0][0]);  //from 0,0 to 0,0 only sum possible is its own
+
+        /*for(int row=1;row<n;row++){
+            //at each of the cell of first col we can arrive from up only
+            dp[row][0]=triangle[row][0]+dp[row-1][0];
+        }*/
+
+        for(int i=1;i<n;i++){
+            vector<int>currRow(i+1);
+            currRow[0]=prevRow[0]+triangle[i][0];
+
+            for(int j=1;j<i+1;j++){
+
+                if(j==i) currRow[j]=triangle[i][j]+prevRow[j-1] ; //we can only come from up-left cell
+
+                else{
+                    int upLeft=triangle[i][j]+prevRow[j-1];
+                    int up=triangle[i][j]+prevRow[j];
+
+                    currRow[j]=min(upLeft,up);
+                }
+            }
+
+            prevRow=currRow;
+        }
+
+        int ans=INT_MAX;
+
+        for(int &i:prevRow){
+            ans=min(ans,i);
+        }
+
+        return ans;
+
+
     }
 };
