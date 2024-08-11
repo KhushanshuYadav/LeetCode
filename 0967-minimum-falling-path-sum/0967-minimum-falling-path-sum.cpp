2d082ece0aff8c25be32cea69efa578fa7a7124d
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-08-11 15:53:04 +0530

    Time: 5 ms (97.84%), Space: 14 MB (18.17%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -20,14 +20,60 @@ private:
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
 
+        //TABULATION
+
         int n=matrix.size();
         int ans=INT_MAX;
 
+        //stores min path sum to cell i,j from any cell in last row i.e last row to cell i,j min path
         vector<vector<int>>dp(n,vector<int>(n,-101));
 
-        for(int j=0;j<n;j++){
-            int temp=solve(0,j,n,matrix,dp);
-            ans=min(ans,temp);
+        //fiiling dp table from last row to first row 
+
+        for(int col=0;col<n;col++){
+            dp[n-1][col]=matrix[n-1][col];
+        }
+
+        //we can arrive to i,j from the downLeft,down,downRight cells so take min of them
+
+        for(int i=n-2;i>=0;i--){
+
+            for(int j=0;j<n;j++){
+
+                if(j==0){
+
+                    int down=matrix[i][j]+dp[i+1][j];
+                    int downRight=matrix[i][j]+dp[i+1][j+1];
+
+                    dp[i][j]=min(down,downRight);
+
+
+
+                }
+                else if(j==n-1){
+
+                    int down=matrix[i][j]+dp[i+1][j];
+                    int downLeft=matrix[i][j]+dp[i+1][j-1];
+
+                    dp[i][j]=min(down,downLeft);
+
+                }
+
+                else{
+
+                    int down=matrix[i][j]+dp[i+1][j];
+                    int downRight=matrix[i][j]+dp[i+1][j+1];
+                    int downLeft=matrix[i][j]+dp[i+1][j-1];
+
+                    dp[i][j]=min({down,downRight,downLeft});
+
+
+                }
+            }
+        }
+
+        for(int& i:dp[0]){
+            ans=min(ans,i);
         }
 
 
