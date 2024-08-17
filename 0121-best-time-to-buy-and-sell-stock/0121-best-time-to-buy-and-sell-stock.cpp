7bf0fd61b8946ae5a52f3f709a1e348b4ea38e3c
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-08-18 01:47:38 +0530

    Time: 85 ms (59.83%), Space: 96.2 MB (19%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,6 +5,8 @@ public:
 
     //code says buy at minimum price so if currr< min update min
     //profit will be max of max profit annd curr profit
+
+    //to sell at ith keep track of min price till i
     int maxProfit=0;
     int minPrice=INT_MAX;
      
