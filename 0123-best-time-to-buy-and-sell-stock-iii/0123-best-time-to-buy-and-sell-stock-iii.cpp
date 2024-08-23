3f2f4589bd57aa1c12a80ea77d78cb0ffe117bf7
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-08-24 02:22:59 +0530

    Time: 173 ms (69.2%), Space: 78.1 MB (88.03%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,37 +1,34 @@
 class Solution {
 
-    //tCount tells transaction count now a full transaction is when we buy and sell stock so 
-    //effectivaly transaction gets completed when we sell a stock so inc then only 
-    //see it as capacity of transaction
+    // tCount tells transaction count now a full transaction is when we buy and
+    // sell stock so effectivaly transaction gets completed when we sell a stock
+    // so inc then only see it as capacity of transaction
 
-    int solve(int i, int canBuy, vector<int>& prices, int tCount , vector<vector<vector<int>>>& dp) {
+    int solve(int i, int canBuy, vector<int>& prices, int tCount,
+              vector<vector<vector<int>>>& dp) {
 
-        if(tCount>=2) return 0;
+        if (tCount >= 2)
+            return 0;
 
+        if (i >= prices.size())
+            return 0;
 
-        
-
-        if (i >= prices.size()) return 0;
-            
-        
-        if(dp[i][canBuy][tCount]!=-1) return dp[i][canBuy][tCount];
-        
-       
-        
+        if (dp[i][canBuy][tCount] != -1)
+            return dp[i][canBuy][tCount];
 
         int profit = 0; // no buy sell so profit is zero initially
 
         if (canBuy) {
 
-            int buy = -prices[i] + solve(i + 1, 0, prices,tCount, dp);
-            int notBuy = 0 + solve(i + 1, 1, prices, tCount,dp);
+            int buy = -prices[i] + solve(i + 1, 0, prices, tCount, dp);
+            int notBuy = 0 + solve(i + 1, 1, prices, tCount, dp);
 
             profit = max(buy, notBuy);
 
         } else {
 
-            int sell = prices[i] + solve(i + 1, 1, prices,tCount+1, dp);
-            int notSell = 0 + solve(i + 1, 0, prices,tCount ,dp);
+            int sell = prices[i] + solve(i + 1, 1, prices, tCount + 1, dp);
+            int notSell = 0 + solve(i + 1, 0, prices, tCount, dp);
 
             profit = max(sell, notSell);
         }
@@ -44,10 +41,51 @@ public:
 
         int n = prices.size();
 
+        // Tabulation by including base cases i.e inc size
+        //SPACE OPTIMIZATION
 
-        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(2,-1)));
+        vector<vector<int>> next(2, vector<int>(3, 0));
+        vector<vector<int>> curr(2, vector<int>(3, 0));
 
-        return solve(0,1,prices,0,dp);
+       
+
+       
+        next[0][2] = 0;
+        next[1][2] = 0;
+
+        curr[0][2] = 0;
+        curr[1][2] = 0;
         
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            for (int canBuy = 0; canBuy <= 1; canBuy++) {
+
+                for (int tCount = 1; tCount >=0 ; tCount--) {
+
+                    int profit = 0; // no buy sell so profit is zero initially
+
+                    if (canBuy) {
+
+                        int buy = -prices[i] + next[0][tCount];
+                        int notBuy = 0 + next[1][tCount];
+
+                        profit = max(buy, notBuy);
+
+                    } else {
+
+                        int sell = prices[i] +next[1][tCount+1];
+                        int notSell = 0 + next[0][tCount];
+
+                        profit = max(sell, notSell);
+                    }
+
+                   curr[canBuy][tCount] = profit;
+                }
+                next=curr;
+            }
+        }
+
+        return next[1][0];
     }
 };
