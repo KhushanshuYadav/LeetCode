175bc6e76bd7c439318a4a65a9cb43fc3740807e
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2024-08-29 20:43:04 +0530

    Time: 4 ms (43.01%), Space: 9.1 MB (79.27%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -6,7 +6,7 @@ public:
             return;
         }
 
-        for(int i=begin;i<nums.size();i++){  //we fix each i and begin position
+        for(int i=begin;i<nums.size();i++){  //we fix each i at begin position
             swap(nums[begin],nums[i]); //swap i.e fix ith and begin
             fun(begin+1,nums,ans);     //call for next not fixed half
             swap(nums[begin],nums[i]); //backtrack
