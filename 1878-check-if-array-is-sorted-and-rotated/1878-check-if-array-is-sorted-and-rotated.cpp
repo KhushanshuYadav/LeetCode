b0676fec9dabe19b018a36b915dbae09b4d31345
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2025-02-02 15:28:47 +0530

    Time: 0 ms (100%), Space: 11 MB (85.59%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -17,37 +17,15 @@ public:
     bool check(vector<int>& nums) {
         int n=nums.size();
 
-        int pivot=-1;
+       int count=0;
 
-        int count=0;
+       for(int i=1;i<n;i++){
 
+        if(nums[i]<nums[i-1]) count++;
         
+       }
+       if(nums[0]<nums[n-1]) count++;
 
-        for(int i=1;i<n;i++){
-
-            if(nums[i]<nums[i-1]) {pivot=i;count++;}
-
-            
-
-        }
-
-        cout<<pivot<<" ";
-        cout<<count;
-
-        if(count>1) return false;
-        if(pivot==-1) return true;
-
-
-        
-        for(int i=pivot;i<n-1;i++){
-
-            if(nums[i]>nums[i+1]) return false;
-        }
-
-        if(nums[n-1]>nums[0]) return false;
-
-        
-
-        return true;
+       return count<=1;
     }
 };
