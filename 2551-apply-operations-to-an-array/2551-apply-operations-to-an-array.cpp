2554e3416d23e8c8746cabe119ebb72f49de10b1
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2025-03-01 16:10:38 +0530

    Time: 0 ms (100%), Space: 13.3 MB (23.58%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.cpp
@@ -5,44 +5,59 @@ public:
         int n=nums.size();
 
         int lastNonZero;
-        vector<int>ans;
+        vector<int>ans(n,-1);
 
-        for(int i=0;i<n-1;i++){
+        int f=0;
+        int b=n-1;
 
-            if(nums[i]==nums[i+1]){
-                nums[i]*=2;
-                nums[i+1]=0;
+        for(int i=0;i<n-1;){
+
+            if(nums[i]!=0){
+
+                if(nums[i]==nums[i+1]){
+                    ans[f]=nums[i]*2;
+                    f++;
+                    ans[b]=0;
+                    b--;
+
+                    i+=2;
+                }
+
+                else{
+                    ans[f]=nums[i];
+                    f++;
+                    i++;
+                }
+            }
+            else{
+
+                ans[b]=0;
+                b--;
+                i++;
+
             }
 
-            if(nums[i]!=0) ans.push_back(nums[i]);
+            
         }
 
-        if(nums[n-1]!=0) ans.push_back(nums[n-1]);
+        if(nums[n-2]==0){
+            if(nums[n-1]==0) ans[b]=0;
+            else ans[f]=nums[n-1];
+        }
+        else{
+            if(nums[n-2]!=nums[n-1]){
+                if(nums[n-1]==0) ans[b]=0;
+                else ans[f]=nums[n-1];
+
+            }
+        }
 
 
 
         
 
-        // if(nums[n-1]!=0) lastNonZero=n-1;
-
-        // for(int i=0;i<n;i++){
-
-        //     if(nums[i]==0){
-        //         swap(nums[i],nums[lastNonZero]);
-        //     }
-
-        //     for(int j=lastNonZero-1;j>=0;j--){
-        //         if(nums[j]!=0) {
-        //             lastNonZero=j;
-        //             break;
-        //         }
-        //     }
-
-        // }
-
-        for(int&i:nums){
-            if(i==0) ans.push_back(i);
-        }
+        
+        
 
         return ans;
         
