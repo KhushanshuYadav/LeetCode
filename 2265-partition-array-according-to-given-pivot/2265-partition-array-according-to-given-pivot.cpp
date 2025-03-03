9ecbc2874e46c022c53d0f0bc674ecd3499705f5
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2025-03-03 21:50:22 +0530

    Time: 9 ms (40.37%), Space: 133.3 MB (61.95%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -5,31 +5,31 @@ public:
         int n = arr.size();
 
         vector<int>ans;
-        vector<int>vis(n,0);
+        //vector<int>vis(n,0);
 
         for(int i=0;i<n;i++){
 
-            if(arr[i]<pivot && !vis[i]) {
+            if(arr[i]<pivot ) {
                 ans.push_back(arr[i]);
-                vis[i]=1;
+                //vis[i]=1;
             }
 
         }
 
         for(int i=0;i<n;i++){
 
-            if(arr[i]==pivot && !vis[i]) {
+            if(arr[i]==pivot  ) {
                 ans.push_back(arr[i]);
-                vis[i]=1;
+                //vis[i]=1;
             }
 
         }
 
         for(int i=0;i<n;i++){
 
-            if(arr[i]>pivot && !vis[i]) {
+            if(arr[i]>pivot ) {
                 ans.push_back(arr[i]);
-                vis[i]=1;
+                //vis[i]=1;
             }
         }
 
