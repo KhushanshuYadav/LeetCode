77427b5f48fbba9d7a2aeb6b15b471335daccc2e
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2025-07-14 19:17:42 +0530

    Time: 0 ms (100%), Space: 10.7 MB (13.59%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -12,38 +12,15 @@ class Solution {
 public:
     int getDecimalValue(ListNode* head) {
 
-        vector<int>TwoPower(30,1);
-
-        for(int i=1;i<30;i++){
-            TwoPower[i]=TwoPower[i-1]*2;
-            cout<<TwoPower[i]<<endl;
-        }
-
-        int n=0;
-        ListNode* temp=head;
-        while(temp){
-            n++;
-            temp=temp->next;
-
-        }
-        cout<<n<<endl;
-
-        temp=head;
-
         int ans=0;
 
-        for(int i=n-1;i>=0;i--){
-
-            if(temp->val!=0) {
-                ans+=((temp->val)*TwoPower[i]);
-                cout<<ans<<endl;
-                }
-                temp=temp->next;
+        while(head){
+            ans=(ans<<1)|head->val;
+            head=head->next;
 
         }
 
         return ans;
-
         
     }
 };
