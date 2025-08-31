946ee0b0d5fa0d47dd7fa30fbbc76cddfb0b4f09
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2025-08-31 18:36:00 +0530

    Time: 539 ms (5.69%), Space: 8.7 MB (62.78%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -5,27 +5,19 @@ public:
     }
     bool isSafe(char num,int row,int col,vector<vector<char>>& board){
 
+        //3=sqrt(9)
+
         
 
         for(int i=0;i<9;i++){
-            if(board[row][i]==num) return false;
+            if(board[row][i]==num ||board[i][col]==num ) return false;
+
+            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==num) return false;
         }
 
-        for(int i=0;i<9;i++){
-            if(board[i][col]==num) return false;
-        }
+        
 
-        int s=sqrt(9);
-
-        int subGridStartRow=row-row%s;
-        int subGridStartCol=col-col%s;
-
-
-        for(int i=0;i<s;i++){
-            for(int j=0;j<s;j++){
-                if(board[i+subGridStartRow][j+subGridStartCol]==num) return false;
-            }
-        }
+        
 
         return true;
 
