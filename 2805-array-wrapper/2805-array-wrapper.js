0f917fe1a69df0c4a8dd02367c65bc08d480adef
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2025-09-14 01:24:53 +0530

    Time: 44 ms (52.34%), Space: 56.7 MB (5.11%) - LeetHub

---
+++ 2805-array-wrapper/2805-array-wrapper.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+
+    this.nums=nums;
+    
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+
+    let num=0;
+    for(let e of this.nums){
+        num+=e;
+    }
+
+    return num;
+
+    
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+
+    return "["+this.nums.toString()+"]";
+    
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
