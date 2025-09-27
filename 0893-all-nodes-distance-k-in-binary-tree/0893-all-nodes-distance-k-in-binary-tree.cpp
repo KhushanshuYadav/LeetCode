d160861eefc78a0237121dd514bdffdcf88ab83b
Khushanshu Yadav <115397654+KhushanshuYadav@users.noreply.github.com>
2025-09-27 21:56:17 +0530

    Time: 4 ms (64.09%), Space: 16.1 MB (16.27%) - LeetHub

---
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+
+    void solve( TreeNode* curr,int d,int& k,unordered_set<TreeNode*>& vis,vector<int>& ans,unordered_map<TreeNode*,TreeNode*>&parent ){
+
+        vis.insert(curr);
+
+        if(d==k) ans.push_back(curr->val);
+
+        if(curr->left && !vis.count(curr->left)) solve(curr->left,d+1,k,vis,ans,parent);
+
+        if(curr->right && !vis.count(curr->right)) solve(curr->right,d+1,k,vis,ans,parent);
+
+        if(parent[curr] && !vis.count(parent[curr])) solve(parent[curr],d+1,k,vis,ans,parent);
+    }
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+
+        unordered_map<TreeNode*,TreeNode*>parent;
+
+        queue<TreeNode*>q;
+
+        TreeNode*temp=nullptr;
+
+        q.push(root);
+
+        while(!q.empty()){
+
+            TreeNode*node=q.front();
+            q.pop();
+
+            if(node->left){
+                parent[node->left]=node;
+                q.push(node->left);
+            }
+
+            if(node->right){
+                parent[node->right]=node;
+                q.push(node->right);
+            }
+        }
+
+
+        TreeNode*curr=target;
+
+        unordered_set<TreeNode*>vis;
+
+        vector<int>ans;
+
+
+        solve(curr,0,k,vis,ans,parent);
+
+        return ans;
+
+
+
+
+        
+    }
+};
